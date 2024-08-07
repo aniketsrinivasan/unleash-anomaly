412c5617cd98f34b084e04523738663016dbb239
Aniket <156108186+aniketsrinivasan@users.noreply.github.com>
2024-08-08 00:26:26 +0100

    Modified AttentionBlock to work with custom number of groups for GroupNorm.

--- models/autoencoders/blocks/attention_block.py
+++ models/autoencoders/blocks/attention_block.py
@@ -10,19 +10,21 @@ class AttentionBlock(nn.Module):
     """
     __num_groups = 8
 
-    def __init__(self, channels: int):
+    def __init__(self, channels: int, num_groups: int = None):
         """
         Performs self-Attention on an image (Tensor) of shape (batch_size, channels, height, width)
         to create relationships between the pixels of an image that are arbitrarily far.
 
         :param channels:    number of channels in the image.
+        :param num_groups:  number of groups in the group normalization (must divide channels).
         """
         super().__init__()
         # Ensuring that the distribution stays somewhat the same (for stability):
         #   group normalization occurs in groups rather than across the entire layer, so each
         #   group has its own mean and variance.
         #   the idea is that we want to normalize, but maintain neighbourhood information.
-        self.groupnorm = nn.GroupNorm(num_groups=self.__num_groups, num_channels=channels)
+        self.num_groups = num_groups if (num_groups is not None) else self.__num_groups
+        self.groupnorm = nn.GroupNorm(num_groups=self.num_groups, num_channels=channels)
         self.attention = SelfAttention(n_heads=1, d_embed=channels)
 
     # Forward method:
@@ -37,13 +39,17 @@ class AttentionBlock(nn.Module):
         # Residue for residual connection:
         residue = x
 
-        # Extracting shape of the input:
-        batch_size, channels, height, width = x.shape
+        is_4d = False
+        batch_size, channels, height, width = None, None, None, None
+        if len(x.shape) == 4:
+            is_4d = True
+            # Extracting shape of the input:
+            batch_size, channels, height, width = x.shape
+            # We perform Self-Attention between all the pixels of the image:
+            #   "flattening" the dimensions of the image
+            #   (batch_size, channels, height, width) ==> (batch_size, channels, height*width)
+            x = x.view(batch_size, channels, height * width)
 
-        # We perform Self-Attention between all the pixels of the image:
-        #   "flattening" the dimensions of the image
-        #   (batch_size, channels, height, width) ==> (batch_size, channels, height*width)
-        x = x.view(batch_size, channels, height * width)
         #   (batch_size, channels, height*width) ==> (batch_size, height*width, channels)
         #       each pixel is in dim=1 (height*width) and has an 'embedding' given by dim=2 (channels).
         #       this is the same idea as used in the Transformer model.
@@ -55,9 +61,11 @@ class AttentionBlock(nn.Module):
         # Transposing back:
         #   (batch_size, height*width, channels) ==> (batch_size, channels, height*width)
         x = x.transpose(dim0=2, dim1=1)
-        # Reforming the Tensor into original shape:
-        #   (batch_size, channels, height*width) ==> (batch_size, channels, height, width)
-        x = x.view((batch_size, channels, height, width))
+
+        if is_4d:
+            # Reforming the Tensor into original shape:
+            #   (batch_size, channels, height*width) ==> (batch_size, channels, height, width)
+            x = x.view((batch_size, channels, height, width))
 
         # Residual connection:
         x += residue
